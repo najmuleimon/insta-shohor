5fdb2df327581aecbd85f494c0068bdd952f1890
najmuleimon <najmul.eimon@gmail.com>
2022-03-11 10:56:51 +0600

    all bug fixed

--- js/app.js
+++ js/app.js
@@ -39,12 +39,14 @@ const switchTab = (id) => {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("questions").style.display = "none";
 
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
+    document.getElementById("questions").style.display = "none";
 
     displayReportedPosts();
   }
